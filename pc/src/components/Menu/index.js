825ad74935186e43bfde76f3cb037e52e084bbea
yangweike <yangweike@hotmail.com>
2018-11-09 10:27:16 +0800

    菜单联动

--- pc/src/components/Menu/index.js
+++ pc/src/components/Menu/index.js
@@ -9,6 +9,18 @@ const SubMenu = Menu.SubMenu;
 class SideMenu extends PureComponent {
 
   render() {
+    const { location } = this.props;
+    const { pathname } = location;
+    const pathList = pathname.split('/');
+    let menuSelectKey = '';
+    if(pathList[pathList.length - 2]==='account'){
+      menuSelectKey = ['account'];
+    }
+    else{
+      menuSelectKey = [pathList[pathList.length - 1]];
+    }
+
+
     return (
       <div style={{height: "calc(100vh)"}}>
         <div className={styles.logo} id="logo" style={{textAlign: "center"}}>
@@ -20,22 +32,22 @@ class SideMenu extends PureComponent {
         <Menu
           mode="inline"
           theme="dark"
-          defaultSelectedKeys={['1']}
+          selectedKeys={menuSelectKey}
           defaultOpenKeys={['index', 'product','order']}
         >
           <SubMenu key="index" title={<span><Icon type="user" /><span>个人页</span></span>}>
-            <Menu.Item key="1"><Link to='/'>我的信息</Link></Menu.Item>
+            <Menu.Item key="account"><Link to='/'>我的信息</Link></Menu.Item>
           </SubMenu>
           <SubMenu key="product" title={<span><Icon type="appstore" /><span>产品管理</span></span>}>
-            <Menu.Item key="2"><Link to='/product/commodity'>商品管理</Link></Menu.Item>
-            <Menu.Item key="3"><Link to='/product/service'>服务管理</Link></Menu.Item>
+            <Menu.Item key="commodity"><Link to='/product/commodity'>商品管理</Link></Menu.Item>
+            <Menu.Item key="service"><Link to='/product/service'>服务管理</Link></Menu.Item>
           </SubMenu>
           <SubMenu key="order" title={<span><Icon type="mail" /><span>订单管理</span></span>}>
-            <Menu.Item key="4"><Link to='/order/pendingpay'>未付款订单</Link></Menu.Item>
-            <Menu.Item key="5"><Link to='/order/pendingsend'>未发货订单</Link></Menu.Item>
-            <Menu.Item key="6"><Link to='/order/delivered'>已发货订单</Link></Menu.Item>
-            <Menu.Item key="7"><Link to='/order/completed'>已完成订单</Link></Menu.Item>
-            <Menu.Item key="8"><Link to='/order/refund'>退款/退货退款订单</Link></Menu.Item>
+            <Menu.Item key="pendingpay"><Link to='/order/pendingpay'>未付款订单</Link></Menu.Item>
+            <Menu.Item key="pendingsend"><Link to='/order/pendingsend'>未发货订单</Link></Menu.Item>
+            <Menu.Item key="delivered"><Link to='/order/delivered'>已发货订单</Link></Menu.Item>
+            <Menu.Item key="completed"><Link to='/order/completed'>已完成订单</Link></Menu.Item>
+            <Menu.Item key="refund"><Link to='/order/refund'>退款/退货退款订单</Link></Menu.Item>
           </SubMenu>
         </Menu>
       </div>
