e0c409fa36b986ab5295c07a59fcc6cb6b03cfe2
wangxiaoqing <xiaoqingwang@126.com>
2018-11-09 21:53:57 +0800

    add

---
+++ pc/src/actions/shop/index.js
@@ -0,0 +1,3 @@
+import { createAction } from 'redux-act';
+export const getproductinfo_request = createAction('getproductinfo_request');
+export const getproductinfo_result = createAction('getproductinfo_result');
